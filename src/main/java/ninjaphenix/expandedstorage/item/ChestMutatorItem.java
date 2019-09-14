d5c55ad77515549b6af10ad396d7bc5d9bd170df
NinjaPhenix <ninjaphenix@hotmail.co.uk>
2019-09-14 13:08:09 +0100

    Gradle 5.6.2, Mutator and modes changes

--- src/main/java/ninjaphenix/expandedstorage/item/ChestMutatorItem.java
+++ src/main/java/ninjaphenix/expandedstorage/item/ChestMutatorItem.java
@@ -5,7 +5,7 @@ import net.fabricmc.api.Environment;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.ChestBlock;
-import net.minecraft.block.EnderChestBlock;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.enums.ChestType;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
@@ -14,6 +14,7 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
 import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.EnumProperty;
 import net.minecraft.state.property.Properties;
@@ -22,7 +23,10 @@ import net.minecraft.text.TranslatableText;
 import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
+import ninjaphenix.expandedstorage.ExpandedStorage;
+import ninjaphenix.expandedstorage.api.Registries;
 import ninjaphenix.expandedstorage.api.block.AbstractChestBlock;
 import ninjaphenix.expandedstorage.api.block.CursedChestBlock;
 import ninjaphenix.expandedstorage.api.block.enums.CursedChestType;
@@ -30,6 +34,10 @@ import ninjaphenix.expandedstorage.api.item.ChestModifierItem;
 
 import java.util.List;
 
+import static net.minecraft.util.BlockRotation.CLOCKWISE_180;
+import static net.minecraft.util.BlockRotation.CLOCKWISE_90;
+
+@SuppressWarnings("ConstantConditions")
 public class ChestMutatorItem extends ChestModifierItem
 {
     private static final DirectionProperty FACING = Properties.HORIZONTAL_FACING;
@@ -106,10 +114,7 @@ public class ChestMutatorItem extends ChestModifierItem
                 switch (mainState.get(CursedChestBlock.TYPE))
                 {
                     case SINGLE:
-                        if (!world.isClient)
-                        {
-                            world.setBlockState(mainBlockPos, mainState.with(FACING, mainState.get(FACING).rotateYClockwise()));
-                        }
+                        if (!world.isClient) world.setBlockState(mainBlockPos, mainState.rotate(CLOCKWISE_90));
                         player.getItemCooldownManager().set(this, 5);
                         return ActionResult.SUCCESS;
                     case FRONT:
@@ -118,10 +123,8 @@ public class ChestMutatorItem extends ChestModifierItem
                     case RIGHT:
                         if (!world.isClient)
                         {
-                            world.setBlockState(mainBlockPos,
-                                    mainState.with(FACING, mainState.get(FACING).getOpposite()).with(TYPE, mainState.get(TYPE).getOpposite()));
-                            world.setBlockState(otherBlockPos,
-                                    otherState.with(FACING, otherState.get(FACING).getOpposite()).with(TYPE, otherState.get(TYPE).getOpposite()));
+                            world.setBlockState(mainBlockPos, mainState.rotate(CLOCKWISE_180).with(TYPE, mainState.get(TYPE).getOpposite()));
+                            world.setBlockState(otherBlockPos, otherState.rotate(CLOCKWISE_180).with(TYPE, otherState.get(TYPE).getOpposite()));
                         }
                         player.getItemCooldownManager().set(this, 5);
                         return ActionResult.SUCCESS;
@@ -129,8 +132,8 @@ public class ChestMutatorItem extends ChestModifierItem
                     case BOTTOM:
                         if (!world.isClient)
                         {
-                            world.setBlockState(mainBlockPos, mainState.with(FACING, mainState.get(FACING).rotateYClockwise()));
-                            world.setBlockState(otherBlockPos, otherState.with(FACING, otherState.get(FACING).rotateYClockwise()));
+                            world.setBlockState(mainBlockPos, mainState.rotate(CLOCKWISE_90));
+                            world.setBlockState(otherBlockPos, otherState.rotate(CLOCKWISE_90));
                         }
                         player.getItemCooldownManager().set(this, 5);
                         return ActionResult.SUCCESS;
@@ -149,67 +152,47 @@ public class ChestMutatorItem extends ChestModifierItem
         MutatorModes mode = getMode(stack);
         if (state.getBlock() instanceof ChestBlock)
         {
+            // todo: Just recode this to be like the new merge method for mod chests.
             if (mode == MutatorModes.MERGE)
             {
-                //Direction direction = context.getSide();
-                //BlockPos otherPos = mainPos.offset(direction);
-                //BlockState otherState = world.getBlockState(otherPos);
-                //Direction facing = state.get(ChestBlock.FACING);
-                //ChestType type;
-                //if (state.getBlock() == otherState.getBlock() && facing == otherState.get(ChestBlock.FACING) &&
-                //        state.get(ChestBlock.CHEST_TYPE) == ChestType.SINGLE && otherState.get(ChestBlock.CHEST_TYPE) == ChestType.SINGLE)
-                //{
-                //    if (facing.rotateYCounterclockwise() == direction)
-                //    {
-                //        type = ChestType.RIGHT;
-                //    }
-                //    else if (facing.rotateYClockwise() == direction)
-                //    {
-                //        type = ChestType.LEFT;
-                //    }
-                //    else
-                //    {
-                //        CursedChestType cursedType;
-                //        if (direction == Direction.UP) cursedType = CursedChestType.BOTTOM;
-                //        else if (direction == Direction.DOWN) cursedType = CursedChestType.TOP;
-                //        else if (facing == direction) cursedType = CursedChestType.BACK;
-                //        else if (facing == direction.getOpposite()) cursedType = CursedChestType.FRONT;
-                //        else return ActionResult.FAIL;
-                //        if (!world.isClient)
-                //        {
-                //            CompoundTag tag = world.getBlockEntity(mainPos).toTag(new CompoundTag());
-                //            ListTag items = tag.getList("Items", 10);
-                //            CompoundTag otherTag = world.getBlockEntity(otherPos).toTag(new CompoundTag());
-                //            ListTag otherItems = otherTag.getList("Items", 10);
-                //            world.removeBlockEntity(mainPos);
-                //            world.setBlockState(mainPos,
-                //                    ModBlocks.wood_chest.getDefaultState().with(CursedChestBlock.TYPE, cursedType).with(CursedChestBlock.FACING, facing)
-                //                                        .with(CursedChestBlock.WATERLOGGED, state.get(ChestBlock.WATERLOGGED)));
-                //            BlockEntity blockEntity = world.getBlockEntity(mainPos);
-                //            tag = blockEntity.toTag(new CompoundTag());
-                //            tag.put("Items", items);
-                //            blockEntity.fromTag(tag);
-                //            world.removeBlockEntity(otherPos);
-                //            world.setBlockState(otherPos, ModBlocks.wood_chest.getDefaultState().with(CursedChestBlock.TYPE, cursedType.getOpposite())
-                //                                                              .with(CursedChestBlock.FACING, facing)
-                //                                                              .with(CursedChestBlock.WATERLOGGED, otherState.get(ChestBlock.WATERLOGGED)));
-                //            BlockEntity otherBlockEntity = world.getBlockEntity(otherPos);
-                //            otherTag = otherBlockEntity.toTag(new CompoundTag());
-                //            otherTag.put("Items", otherItems);
-                //            otherBlockEntity.fromTag(otherTag);
-                //        }
-                //        player.getItemCooldownManager().set(this, 5);
-                //        return ActionResult.SUCCESS;
-                //    }
-                //    if (!world.isClient)
-                //    {
-                //        world.setBlockState(mainPos, state.with(ChestBlock.CHEST_TYPE, type));
-                //        world.setBlockState(otherPos, otherState.with(ChestBlock.CHEST_TYPE, type.getOpposite()));
-                //    }
-                //    player.getItemCooldownManager().set(this, 5);
-                //    return ActionResult.SUCCESS;
-                //}
-                //return ActionResult.FAIL;
+                Direction direction = context.getSide();
+                BlockPos otherPos = mainPos.offset(direction);
+                BlockState otherState = world.getBlockState(otherPos);
+                Direction facing = state.get(ChestBlock.FACING);
+                if (state.getBlock() == otherState.getBlock() && facing == otherState.get(ChestBlock.FACING) &&
+                        state.get(ChestBlock.CHEST_TYPE) == ChestType.SINGLE && otherState.get(ChestBlock.CHEST_TYPE) == ChestType.SINGLE)
+                {
+                    CursedChestType cursedType;
+                    cursedType = AbstractChestBlock.getChestType(facing, direction);
+                    if (cursedType == CursedChestType.SINGLE) return ActionResult.FAIL;
+                    if (!world.isClient)
+                    {
+                        BlockState defaultState = Registry.BLOCK.get(Registries.MODELED.get(ExpandedStorage.getId("wood_chest")).getBlockId())
+                                                                .getDefaultState();
+                        CompoundTag tag = world.getBlockEntity(mainPos).toTag(new CompoundTag());
+                        ListTag items = tag.getList("Items", 10);
+                        CompoundTag otherTag = world.getBlockEntity(otherPos).toTag(new CompoundTag());
+                        ListTag otherItems = otherTag.getList("Items", 10);
+                        world.removeBlockEntity(mainPos);
+                        world.removeBlockEntity(otherPos);
+                        world.setBlockState(mainPos, defaultState.with(CursedChestBlock.TYPE, cursedType).with(CursedChestBlock.FACING, facing)
+                                                                 .with(CursedChestBlock.WATERLOGGED, state.get(ChestBlock.WATERLOGGED)));
+                        world.setBlockState(otherPos, defaultState.with(CursedChestBlock.TYPE, cursedType.getOpposite())
+                                                                  .with(CursedChestBlock.FACING, facing)
+                                                                  .with(CursedChestBlock.WATERLOGGED, otherState.get(ChestBlock.WATERLOGGED)));
+                        BlockEntity blockEntity = world.getBlockEntity(mainPos);
+                        tag = blockEntity.toTag(new CompoundTag());
+                        tag.put("Items", items);
+                        blockEntity.fromTag(tag);
+                        BlockEntity otherBlockEntity = world.getBlockEntity(otherPos);
+                        otherTag = otherBlockEntity.toTag(new CompoundTag());
+                        otherTag.put("Items", otherItems);
+                        otherBlockEntity.fromTag(otherTag);
+                    }
+                    player.getItemCooldownManager().set(this, 5);
+                    return ActionResult.SUCCESS;
+                }
+                return ActionResult.FAIL;
             }
             else if (mode == MutatorModes.UNMERGE)
             {
@@ -245,7 +228,7 @@ public class ChestMutatorItem extends ChestModifierItem
                         otherPos = mainPos.offset(state.get(ChestBlock.FACING).rotateYCounterclockwise());
                         break;
                     case SINGLE:
-                        if (!world.isClient) world.setBlockState(mainPos, state.rotate(BlockRotation.CLOCKWISE_90));
+                        if (!world.isClient) world.setBlockState(mainPos, state.rotate(CLOCKWISE_90));
                         player.getItemCooldownManager().set(this, 5);
                         return ActionResult.SUCCESS;
                     default:
@@ -254,9 +237,9 @@ public class ChestMutatorItem extends ChestModifierItem
                 if (!world.isClient)
                 {
                     BlockState otherState = world.getBlockState(otherPos);
-                    world.setBlockState(mainPos, state.rotate(BlockRotation.CLOCKWISE_180)
+                    world.setBlockState(mainPos, state.rotate(CLOCKWISE_180)
                                                       .with(ChestBlock.CHEST_TYPE, state.get(ChestBlock.CHEST_TYPE).getOpposite()));
-                    world.setBlockState(otherPos, otherState.rotate(BlockRotation.CLOCKWISE_180)
+                    world.setBlockState(otherPos, otherState.rotate(CLOCKWISE_180)
                                                             .with(ChestBlock.CHEST_TYPE, otherState.get(ChestBlock.CHEST_TYPE).getOpposite()));
                 }
                 player.getItemCooldownManager().set(this, 5);
@@ -265,9 +248,9 @@ public class ChestMutatorItem extends ChestModifierItem
         }
         else if (state.getBlock() == Blocks.ENDER_CHEST)
         {
-            if (getMode(stack) == MutatorModes.ROTATE)
+            if (mode == MutatorModes.ROTATE)
             {
-                if (!world.isClient) world.setBlockState(mainPos, state.rotate(BlockRotation.CLOCKWISE_90));
+                if (!world.isClient) world.setBlockState(mainPos, state.rotate(CLOCKWISE_90));
                 player.getItemCooldownManager().set(this, 5);
                 return ActionResult.SUCCESS;
             }
@@ -282,10 +265,8 @@ public class ChestMutatorItem extends ChestModifierItem
         if (player.isSneaking())
         {
             ItemStack stack = player.getStackInHand(hand);
-            byte mode = (byte) getMode(stack).ordinal();
-            if (++mode >= MutatorModes.values().length) mode = 0;
             CompoundTag tag = stack.getOrCreateTag();
-            tag.putByte("mode", mode);
+            tag.putByte("mode", getMode(stack).next);
             if (tag.containsKey("pos")) tag.remove("pos");
             if (!world.isClient) player.addChatMessage(getMode(stack).translation, true);
             return new TypedActionResult<>(ActionResult.SUCCESS, stack, false);
@@ -317,12 +298,8 @@ public class ChestMutatorItem extends ChestModifierItem
     private MutatorModes getMode(ItemStack stack)
     {
         CompoundTag tag = stack.getOrCreateTag();
-        if (tag.containsKey("mode", 1)) return MutatorModes.values()[tag.getByte("mode")];
-        else
-        {
-            tag.putByte("mode", (byte) 0);
-            return MutatorModes.ROTATE;
-        }
+        if (!tag.containsKey("mode", 1)) tag.putByte("mode", (byte) 0);
+        return MutatorModes.values()[tag.getByte("mode")];
     }
 
     @Environment(EnvType.CLIENT)
--- src/main/java/ninjaphenix/expandedstorage/item/MutatorModes.java
+++ src/main/java/ninjaphenix/expandedstorage/item/MutatorModes.java
@@ -4,14 +4,15 @@ import net.minecraft.text.TranslatableText;
 
 public enum MutatorModes
 {
-    ROTATE(new TranslatableText("tooltip.expandedstorage.chest_mutator.rotate")),
-    MERGE(new TranslatableText("tooltip.expandedstorage.chest_mutator.merge")),
-    UNMERGE(new TranslatableText("tooltip.expandedstorage.chest_mutator.unmerge"));
+    ROTATE(new TranslatableText("tooltip.expandedstorage.chest_mutator.rotate"), 1),
+    MERGE(new TranslatableText("tooltip.expandedstorage.chest_mutator.merge"), 2),
+    UNMERGE(new TranslatableText("tooltip.expandedstorage.chest_mutator.unmerge"), 0);
 
     public final TranslatableText translation;
+    public final byte next;
 
-    MutatorModes(TranslatableText translation)
+    MutatorModes(TranslatableText translation, int next)
     {
-        this.translation = translation;
+        this.translation = translation; this.next = (byte) next;
     }
 }
