06f920640c2e458772f50530065d6240702b05ea
NinjaPhenix <ninjaphenix@hotmail.co.uk>
2019-09-16 08:05:40 +0100

    Fixed chest mutator default mode being rotate

--- src/main/java/ninjaphenix/expandedstorage/item/MutatorModes.java
+++ src/main/java/ninjaphenix/expandedstorage/item/MutatorModes.java
@@ -4,9 +4,9 @@ import net.minecraft.text.TranslatableText;
 
 public enum MutatorModes
 {
-    ROTATE(new TranslatableText("tooltip.expandedstorage.chest_mutator.rotate"), 1),
-    MERGE(new TranslatableText("tooltip.expandedstorage.chest_mutator.merge"), 2),
-    UNMERGE(new TranslatableText("tooltip.expandedstorage.chest_mutator.unmerge"), 0);
+    MERGE(new TranslatableText("tooltip.expandedstorage.chest_mutator.merge"), 1),
+    UNMERGE(new TranslatableText("tooltip.expandedstorage.chest_mutator.unmerge"), 2),
+    ROTATE(new TranslatableText("tooltip.expandedstorage.chest_mutator.rotate"), 0);
 
     public final TranslatableText translation;
     public final byte next;
