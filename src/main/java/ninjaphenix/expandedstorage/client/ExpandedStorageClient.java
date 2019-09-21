de23bac524281240e8073cc66286376ddafb8bdb
NinjaPhenix <ninjaphenix@hotmail.co.uk>
2019-09-21 12:23:22 +0100

    Actually ingame 19w38b EVERYTHING renders wrong.

--- src/main/java/ninjaphenix/expandedstorage/client/ExpandedStorageClient.java
+++ src/main/java/ninjaphenix/expandedstorage/client/ExpandedStorageClient.java
@@ -3,8 +3,8 @@ package ninjaphenix.expandedstorage.client;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-//import net.fabricmc.fabric.api.client.render.BlockEntityRendererRegistry;
-//import net.fabricmc.fabric.api.client.screen.ScreenProviderRegistry;
+import net.fabricmc.fabric.api.client.render.BlockEntityRendererRegistry;
+import net.fabricmc.fabric.api.client.screen.ScreenProviderRegistry;
 import ninjaphenix.expandedstorage.ExpandedStorage;
 import ninjaphenix.expandedstorage.api.block.entity.CursedChestBlockEntity;
 import ninjaphenix.expandedstorage.api.client.gui.screen.ingame.ScrollableScreen;
@@ -16,7 +16,7 @@ public class ExpandedStorageClient implements ClientModInitializer
     @Override
     public void onInitializeClient()
     {
-        //BlockEntityRendererRegistry.INSTANCE.register(CursedChestBlockEntity.class, new CursedChestRenderer());
-        //ScreenProviderRegistry.INSTANCE.registerFactory(ExpandedStorage.getId("scrollcontainer"), ScrollableScreen::createScreen);
+        BlockEntityRendererRegistry.INSTANCE.register(CursedChestBlockEntity.class, new CursedChestRenderer());
+        ScreenProviderRegistry.INSTANCE.registerFactory(ExpandedStorage.getId("scrollcontainer"), ScrollableScreen::createScreen);
     }
 }
