e2d99da6248f16485b716a39660fab744895cc77
NinjaPhenix <ninjaphenix@hotmail.co.uk>
2019-09-27 19:14:01 +0100

    Add IdentifierMixin which allows for backwards compat (will test more)

---
+++ src/main/java/ninjaphenix/expandedstorage/mixins/IdentifierMixin.java
@@ -0,0 +1,22 @@
+package ninjaphenix.expandedstorage.mixins;
+
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Identifier.class)
+public class IdentifierMixin
+{
+    @Mutable @Shadow @Final protected String namespace;
+
+    @Inject(method = "<init>([Ljava/lang/String;)V", at = @At("RETURN"))
+    private void r(String[] components, CallbackInfo ci)
+    {
+        if (namespace.equals("cursedchests")) namespace = "expandedstorage";
+    }
+}
